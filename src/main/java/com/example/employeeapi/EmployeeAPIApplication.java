4077ab0dabb1d78ae00fd0bacec228b18b64b569
llr392425595 <392425595@qq.com>
2018-04-11 16:26:15 +0800

    恢复入口文件

--- src/main/java/com/example/employeeapi/EmployeeAPIApplication.java
+++ src/main/java/com/example/employeeapi/EmployeeAPIApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EmployeeAPIApplication {
+ public class EmployeeAPIApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeeAPIApplication.class, args);
